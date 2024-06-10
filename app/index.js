c50d20784008d2399bfa965d5fdecffce0e2b01a
XUjiayi777 <jxugl777@gmail.com>
2024-06-10 15:36:21 +0800

    dev set up

---
+++ app/index.js
@@ -0,0 +1,16 @@
+// This file shows the actual home page
+import {useState} from 'react';
+import {View,Text,ScrollView,SafeAreaView} from 'react-native';
+import {Stack,useRouter} from 'expo-router';
+
+import {COLORS,icons,images,SIZES} from '../constants';
+
+const Home =()=>{
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
